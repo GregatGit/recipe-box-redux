55eb0f194e93f8e95b996789816759500bdb4ea0
Greg Duncan <gregdd@outlook.com>
2018-04-17 15:40:10 +1000

    add button to change view

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import RecipeList from './components/recipe_list'
 import RecipeListAccordian from './components/recipe_list_accordian'
+import AddRecipe from './components/recipe_add'
 
 class App extends Component {
   constructor (props) {
@@ -9,6 +10,11 @@ class App extends Component {
       showRecipeButton: true
     }
   }
+  changeView = () => {
+    console.log('change view')
+    let temp = !this.state.showRecipeButton
+    this.setState({showRecipeButton: temp})
+  }
   
   render() {
     return (
@@ -16,8 +22,7 @@ class App extends Component {
         <div>
           <h1>Recipe Box</h1>
         </div>
-        <RecipeListAccordian />
-        {this.state.showRecipeButton ? <button className='btn btn-primary'>add recipe</button> : ''}
+        {this.state.showRecipeButton ? <div><RecipeListAccordian /><button onClick={this.changeView} className='btn btn-primary'>add recipe</button></div> : <div><AddRecipe /><button onClick={this.changeView} className='btn btn-danger'>Cancel</button></div>}
       </div>
     );
   }
---
+++ src/components/recipe_add.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+//import { connect } from 'react-redux'
+
+class AddRecipe extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div>new recipe</div>
+    )
+  }  
+}
+
+
+
+export default AddRecipe
