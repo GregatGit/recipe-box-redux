51af1ec726b9f7f31a0d26c4cc789400d0b89f76
Greg Duncan <gregdd@outlook.com>
2018-04-17 16:23:58 +1000

    add arr of ingredient to display

--- src/components/recipe_add.js
+++ src/components/recipe_add.js
@@ -4,14 +4,42 @@ import React, { Component } from 'react'
 class AddRecipe extends Component {
   constructor (props) {
     super(props)
+    this.state = {
+      name: '',
+      nextIngredient: '',
+      ingredients: ['apple', 'pears']
+    }
   }
+  handleName = (event) => {
+    this.setState({
+      name: event.target.value
+    })
+  }
+
+  showIngredients = () => {
+    return (
+      <ul>
+        {this.state.ingredients.map(ingredient => {
+          return <li>{ingredient}</li>
+        })}
+      </ul>
+    )
+  }
+
   render() {
     return (
-      <div>new recipe</div>
+      <div>
+        <h3>New Recipe</h3>
+        <input 
+          value={this.state.name}
+          onChange={this.handleName}
+          placeholder='name'
+        />
+        <p>Name: {this.state.name}</p>
+        <p>ingredients: {this.showIngredients()}</p>
+      </div>
     )
   }  
 }
-
-
 
 export default AddRecipe
