321c908c7874e225ee4ca48a59c7b86ad3778612
Greg Duncan <gregdd@outlook.com>
2018-04-20 14:04:40 +1000

    remove unused files and packages

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux'
 import RecipeReducer from './reducer_recipes'
-import { reducer as formReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
-  recipes: RecipeReducer,
-  form: formReducer
+  recipes: RecipeReducer
 })
 
 export default rootReducer
